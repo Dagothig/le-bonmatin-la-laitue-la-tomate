45f8b611d14b4bb3ae6826ea05809469b84878fc
dagothig <guillaumenoelmartel@gmail.com>
2022-05-29 22:11:18 -0400

    Dans la foret des malgaboués

--- js/plugins.js
+++ js/plugins.js
@@ -6,6 +6,7 @@ var $plugins =
 {"name":"GALV_BasicEventShadows","status":true,"description":"(v.1.2) Display shadows under the player or events on the map.","parameters":{"Shadow Image":"Shadow1","Shadow Offset":"5"}},
 {"name":"GALV_DisableDash","status":true,"description":"Disable dash in your game.","parameters":{"Disable Option":"true"}},
 {"name":"AnimatedSplashScreens","status":true,"description":"Set up animated splash screens that show before the title screen.","parameters":{}},
+{"name":"Foreground","status":true,"description":"display foreground tiling sprite(Ver1.0.1)","parameters":{}},
 {"name":"TitleCommandPosition","status":true,"description":"Changes the position of the title command window.","parameters":{"Offset X":"-160","Offset Y":"15","Width":"305","Background":"2"}},
 {"name":"AltMenuScreen","status":false,"description":"Alternative menu screen layout.","parameters":{}},
 {"name":"Irina_PerformanceUpgrade","status":true,"description":"<PerformanceUpgrade> for RPG Maker MV version 1.6.2.","parameters":{"":"","AnimationHue":"true","BlurMenuBackground":"true","BlurIntensity":"0.5","CacheTextColors":"true","EnemyHue":"true","PixiContainerFlush":"true","SkipUnnecessarySnapshots":"true"}},
